9d4cae373864af272858efa135af5f17015a75c8
povarna <povarna@adobe.com>
2026-02-02 23:48:20 +0200

    AoC problem

--- 01-go-fundamentals/aoc/2021/day03/problem.go
+++ 01-go-fundamentals/aoc/2021/day03/problem.go
@@ -21,6 +21,7 @@ func Run() {
 
 	input := string(bytes)
 	fmt.Printf("AoC2021, Day1, Part1 solution is: %d\n", part1(&input))
+	fmt.Printf("AoC2021, Day1, Part2 solution is: %d\n", part2(&input))
 }
 
 func part1(input *string) int {
@@ -59,4 +60,66 @@ func part1(input *string) int {
 		panic(err)
 	}
 	return int(e * g)
+}
+
+func part2(input *string) int {
+
+	lines := strings.Split(*input, "\n")
+
+	for i := 0; i < len(lines[0]) && len(lines) > 1; i++ {
+		countOnes, countZeros := 0, 0
+		for _, line := range lines {
+			if line[i] == '1' {
+				countOnes += 1
+			} else {
+				countZeros += 1
+			}
+		}
+		if countOnes >= countZeros {
+			lines = filter_lines(lines, i, '1')
+		} else {
+			lines = filter_lines(lines, i, '0')
+		}
+	}
+
+	oxygenGeneratorRating := lines[0]
+	oxygenGeneratorRatingInt, err := strconv.ParseInt(oxygenGeneratorRating, 2, 64)
+	if err != nil {
+		panic(err)
+	}
+
+	lines = strings.Split(*input, "\n")
+	for i := 0; i < len(lines[0]) && len(lines) > 1; i++ {
+		countOnes, countZeros := 0, 0
+		for _, line := range lines {
+			if line[i] == '1' {
+				countOnes += 1
+			} else {
+				countZeros += 1
+			}
+		}
+		if countOnes >= countZeros {
+			lines = filter_lines(lines, i, '0')
+		} else {
+			lines = filter_lines(lines, i, '1')
+		}
+	}
+
+	co2ScrubberRating := lines[0]
+	co2ScrubberRatingInt, err := strconv.ParseInt(co2ScrubberRating, 2, 64)
+	if err != nil {
+		panic(err)
+	}
+
+	return int(oxygenGeneratorRatingInt * co2ScrubberRatingInt)
+}
+
+func filter_lines(lines []string, index int, value rune) []string {
+	new_lines := []string{}
+	for _, line := range lines {
+		if rune(line[index]) == value {
+			new_lines = append(new_lines, line)
+		}
+	}
+	return new_lines
 }
